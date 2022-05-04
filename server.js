2b3b2ec74efea18ae5d86961abbcb499400bc822
tamir <timorust1@gmail.com>
2022-05-04 11:55:00 +0300

    morgan

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
 
 const app = express();
 
-app.set('ejs-views', 'ejs');
+app.set("ejs-views", "ejs");
 
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
+const createPath = page => path.resolve(__dirname, "ejs-views", `${page}.ejs`);
 
-app.listen(PORT, (error) => {
-	error ? console.log(error) : console.log(`Listening port ${PORT}`);
-})
+app.listen(PORT, error => {
+  error ? console.log(error) : console.log(`Listening port ${PORT}`);
+});
 
-app.get('/', (req, res) => {
-	const title = 'Home';
-	res.render(createPath('index'), { title });
-})
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
+
+app.use(express.static("styles"));
+
+app.get("/", (req, res) => {
+  const title = "Home";
+  res.render(createPath("index"), { title });
+});
 
 app.get("/contacts", (req, res) => {
-	const title = "Contacts";
+  const title = "Contacts";
 
-	const contacts = [
+  const contacts = [
     { name: "YouTube", link: "https://youtu.be/_sVRBhf8txY" },
     { name: "Twitter", link: "https://twitter.com/timor61994173" },
     { name: "GitHub", link: "https://github.com/timorust" },
@@ -31,25 +38,21 @@ app.get("/contacts", (req, res) => {
 });
 
 app.get("/posts/:id", (req, res) => {
-	const title = "Post";
+  const title = "Post";
   res.render(createPath("post"), { title });
 });
 
 app.get("/posts", (req, res) => {
-	const title = "Posts";
+  const title = "Posts";
   res.render(createPath("posts"), { title });
 });
 
 app.get("/add-post", (req, res) => {
-	const title = "Add post";
+  const title = "Add post";
   res.render(createPath("add-post"), { title });
 });
 
 app.use((req, res) => {
-	const title = "Error Page";
+  const title = "Error Page";
   res.status(404).render(createPath("error"), { title });
 });
-
-
-
-
