cbc5effe41980fe1e1f6ee1b9845fd145b02a8de
tamir <timorust1@gmail.com>
2022-05-05 19:43:12 +0300

    mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
 const app = express();
 
 app.set("ejs-views", "ejs");
 
 const PORT = 3000;
+
+const db =
+  "mongodb+srv://timor:timor321@cluster1.rchig.mongodb.net/node-blog?retryWrites=true&w=majority";
+  
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(res => console.log("Connect to DB"))
+  .catch(error => console.log(error));
 
 const createPath = page => path.resolve(__dirname, "ejs-views", `${page}.ejs`);
 
@@ -72,15 +82,15 @@ app.get("/posts", (req, res) => {
 app.post('/add-post', (req, res) => {
 	const { title, author, text } = req.body;
 
-	const post = {
-		id: new Date(),
-		date: (new Date()).toLocaleDateString(),
-		title,
-		author,
-		text,
-	};
+	const post = new Post({ title, author, text });
 
-	res.render(createPath('post'), { post, title });
+	post
+    .save()
+    .then(result => res.send(result))
+    .catch(error => {
+      console.log(error);
+      res.render(createPath("error"), { title: "Error" });
+    });
 })
 
 app.get("/add-post", (req, res) => {
